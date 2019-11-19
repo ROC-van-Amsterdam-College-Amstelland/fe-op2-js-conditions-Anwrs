b65760f5251ce92d52d1bbee969948c2c560edad
anouar el ghazaoui <anouar.eg@live.nl>
2019-11-19 12:31:31 +0100

    done

---
+++ Loops/script.js
@@ -0,0 +1,13 @@
+function geefGetallen() {
+    var ingevuldGetal = document.getElementById("startgetal").value; 
+
+
+    for(var teller = 1; teller <= 10; teller += 1) {
+
+
+        console.log(ingevuldGetal * teller);
+        
+        document.getElementById("antwoord").innerHTML = (ingevuldGetal * teller);
+    }
+
+}
---
+++ hw-opdracht1/script.js
@@ -0,0 +1,38 @@
+function bereken() {
+    var getal1;
+    var getal2;
+
+    getal1 = document.getElementById("invoerveld1").value;
+    getal2 = document.getElementById("invoerveld2").value;
+
+    // alert(getal1);
+    // alert(getal2);
+
+    if (getal1 > getal2) {
+        
+        var total = Number(getal1) + Number(getal2);
+        
+    }
+
+    if (getal1 <= getal2) {
+        
+        var total = Number(getal1) - Number(getal2);
+
+    }
+
+    if (getal1 == 10)  (getal1 <= getal2); {
+        
+        var total = Number(getal1) + Number(getal2);
+
+    }
+
+    if (getal1 == 5)  (getal1 >= getal2); {
+        
+        var total = Number(getal1) == Number(getal2);
+
+    }
+
+
+
+    alert(total);
+}
